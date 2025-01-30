750f5e343b89c215ab3fce372dc143ccb6c260aa
Arthur Murray <arthur.dartora.tds2023@gmail.com>
2025-01-30 15:46:02 -0300

    Moto.php

--- TRABALHO/modelos/Moto.php
+++ TRABALHO/modelos/Moto.php
@@ -1,1 +1,110 @@
+<?php 
 
+require_once("Veiculos.php");
+
+Class Moto extends Veiculos{
+    private $marca;
+    private $modelo;
+    private $potencia;
+    private $cor;
+    private $ano;
+
+    
+
+    public function __toString()
+    {
+        
+        return "O Modelo: " . $this->modelo . " da Marca: " . $this->marca . " de potencia: " . $this->potencia . " de cor: " . $this->cor. " de potência: " . $this->ano;
+        
+    }
+
+    /**
+     * Get the value of marca
+     */
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    /**
+     * Set the value of marca
+     */
+    public function setMarca($marca): self
+    {
+        $this->marca = $marca;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of modelo
+     */
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    /**
+     * Set the value of modelo
+     */
+    public function setModelo($modelo): self
+    {
+        $this->modelo = $modelo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of potencia
+     */
+    public function getPotencia()
+    {
+        return $this->potencia;
+    }
+
+    /**
+     * Set the value of potencia
+     */
+    public function setPotencia($potencia): self
+    {
+        $this->potencia = $potencia;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cor
+     */
+    public function getCor()
+    {
+        return $this->cor;
+    }
+
+    /**
+     * Set the value of cor
+     */
+    public function setCor($cor): self
+    {
+        $this->cor = $cor;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ano
+     */
+    public function getAno()
+    {
+        return $this->ano;
+    }
+
+    /**
+     * Set the value of ano
+     */
+    public function setAno($ano): self
+    {
+        $this->ano = $ano;
+
+        return $this;
+    }
+}
